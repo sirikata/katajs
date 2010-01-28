3cb4a8fb41a08125be5a88ac600e92e891e9bbe2
danx0r <danbmil99@gmail.com>
2010-01-28 15:13:39 -0800

    nifty metaprogramming -- makeDraggable

--- k3d.js
+++ k3d.js
@@ -2,6 +2,15 @@
  * @author dbm
  */
 
+function pdebug(s, l){
+	if (l === undefined) {
+		document.getElementById("dbg").innerHTML += s + "<br>"
+	}
+	else {
+		document.getElementById("dbg" + l).innerHTML = s + "<br>"	
+	}
+}
+
 function addProtoSafely(cls, proto, func){
     if (cls.prototype[proto] != null) {
         alert("oh no! This prototype already exists: " + proto)
@@ -10,6 +19,17 @@ function addProtoSafely(cls, proto, func){
         cls.prototype[proto] = func
     }
 }
+
+// add getObjectById method to Scene
+
+addProtoSafely(GLGE.Scene, "getObjectById", function(id) {
+	for(var i=0; i<this.objects.length; i++) {
+		if (this.objects[i].id==id) return this.objects[i]
+	}
+	return null
+})
+
+// add setStatus method to Object
 
 addProtoSafely(GLGE.Object, "setStatus", function(status) {
     if (status != this.status) {
@@ -26,4 +46,28 @@ addProtoSafely(GLGE.Object, "setStatus", function(status) {
         }
 		this.status = status
     }
+})
+
+// add makeDragMovable to Object.  One of a series of draggable options maybe?
+
+addProtoSafely(GLGE.Object, "makeDragMovable", function() {
+	this.dragable = true
+	pdebug("makeDragMovable on " + this.id + " dragable: " + this.dragable)
+	this.dragStart = function (mouse_x, mouse_y) {
+		pdebug("mouse_x type: " + typeof(mouse_x))
+		this.dragStartMouseX = mouse_x
+		this.dragStartMouseY = mouse_y
+		this.dragStartLocX = parseFloat(this.getLocX())			///	wtf?
+		this.dragStartLocZ = parseFloat(this.getLocZ())		
+		pdebug("dragStart x: " + this.dragStartMouseX + " y: " + this.dragStartMouseY + " types: " +
+			typeof(mouse_x)+typeof(mouse_y)+typeof(this.dragStartLocX)+typeof(this.dragStartLocY))
+	}
+
+	this.dragUpdate = function (mouse_x, mouse_y) {
+		move_x = mouse_x-this.dragStartMouseX
+		move_y = mouse_y-this.dragStartMouseY
+		this.setLocX(this.dragStartLocX-move_x*.04)
+		this.setLocZ(this.dragStartLocZ-move_y*.04)
+		pdebug("dragUpdate x: " + this.getLocX() + " y: " + this.getLocZ(),4)
+	}
 })
