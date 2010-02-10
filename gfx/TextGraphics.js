c9b8c987cda34c9b4a91a237f8ee208295328bf8
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-02-09 19:26:50 -0800

    added z index and mesh style

--- gfx/TextGraphics.js
+++ gfx/TextGraphics.js
@@ -19,16 +19,18 @@ TextGraphics=function(callbackFunction,parentElement) {
     }
     this.methodTable["Create"]=function(msg) {
         var div=document.createElement("div");
-        div.style.width="288px";
-        div.style.height="512px";
+        div.style.width="256px";
+        div.style.height="256px";
         div.style.padding="0.5em";
         div.style.position="absolute"
         div.style.border="solid 10px #10107c"
         div.style.backgroundColor="#000008"; 
+        div.style.color="#ffffff"; 
         div.style.left=msg.pos[0]+"px";
         div.style.top=msg.pos[1]+"px";
+        div.style.zIndex=msg.pos[2];
         div.id=msg.id;
-        if (typeof(msg.parent)!==undefined) {
+        if (msg.parent) {
             element=returnObjById(msg.parent);
             if (element) {
                 element.appendChild(div);
@@ -44,11 +46,35 @@ TextGraphics=function(callbackFunction,parentElement) {
         element=returnObjById(msg.id);
         element.style.left=msg.pos[0]+"px";
         element.style.top=msg.pos[1]+"px";
+        element.style.zIndex=msg.pos[2];
     }
     this.methodTable["Destroy"]=function(msg) {
         var div=returnObjById(msg.id);
         if (div) {
             div.parentNode.removeChild(div);        
+        }
+    }
+    this.methodTable["Mesh"]=function(msg) {
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=returnObjById("Mesh"+msg.id);
+            if (q) {
+                
+            }else {
+                q=document.createElement("p");
+                q.id="Mesh"+msg.id;
+            }
+            q.innerHTML="Mesh "+msg.mesh;
+            div.appendChild(q);
+        }        
+    }
+    this.methodTable["DestroyMesh"]=function(msg) {
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=returnObjById("Mesh"+msg.id);
+            if (q) {
+                div.removeChild(q);
+            }
         }
     }
     this.send=function(obj) {
