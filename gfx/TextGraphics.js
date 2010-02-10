6b9593bec507a04e838835f832db1963de2cd8a8
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-02-09 19:53:06 -0800

    silly example finished

--- gfx/TextGraphics.js
+++ gfx/TextGraphics.js
@@ -19,9 +19,9 @@ TextGraphics=function(callbackFunction,parentElement) {
     }
     this.methodTable["Create"]=function(msg) {
         var div=document.createElement("div");
-        div.style.width="256px";
-        div.style.height="256px";
-        div.style.padding="0.5em";
+        div.style.width="300px";
+        div.style.height="300px";
+        div.style.padding="0.0em";
         div.style.position="absolute"
         div.style.border="solid 10px #10107c"
         div.style.backgroundColor="#000008"; 
@@ -54,29 +54,99 @@ TextGraphics=function(callbackFunction,parentElement) {
             div.parentNode.removeChild(div);        
         }
     }
+    var getOrCreateP=function(elementName) {
+        var q=returnObjById(elementName);        
+        if(q) {
+            
+        }else {
+            q=document.createElement("p");
+            q.id=elementName;
+        }
+        return q;
+    }
+    this.methodTable["MeshShaderUniform"]=function(msg) {
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=getOrCreateP("Uniform"+msg.name+msg.id);
+            q.innerHTML="Uniform "+msg.name+"="+msg.value;
+            div.appendChild(q);
+        }        
+    }
     this.methodTable["Mesh"]=function(msg) {
         var div=returnObjById(msg.id);
         if (div) {
-            var q=returnObjById("Mesh"+msg.id);
-            if (q) {
-                
-            }else {
-                q=document.createElement("p");
-                q.id="Mesh"+msg.id;
-            }
+            var q=getOrCreateP("Mesh"+msg.id);
             q.innerHTML="Mesh "+msg.mesh;
             div.appendChild(q);
         }        
     }
-    this.methodTable["DestroyMesh"]=function(msg) {
+    var destroyX=function(msg,X) {
         var div=returnObjById(msg.id);
         if (div) {
-            var q=returnObjById("Mesh"+msg.id);
+            var q=returnObjById(X+msg.id);
             if (q) {
                 div.removeChild(q);
             }
         }
     }
+    this.methodTable["DestroyMesh"]=function(msg) {
+        destroyX(msg,"Mesh");
+    }
+    this.methodTable["Light"]=function(msg) {
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=getOrCreateP("Light"+msg.id);
+            q.innerHTML="Light "+msg.type;
+            div.appendChild(q);
+        }        
+    }
+    this.methodTable["DestroyLight"]=function(msg) {
+        destroyX(msg,"Light");
+    }
+    
+    this.methodTable["Camera"]=function(msg) {
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=getOrCreateP("Camera"+msg.id);
+            q.innerHTML="Camera "+msg.primary;
+            div.appendChild(q);
+        }        
+    }
+    this.methodTable["AttachCamera"]=function(msg) {
+        if (msg.camid) {
+            var div=returnObjById(msg.id);
+            if (div) {
+                var q=getOrCreateP(msg.texname+"CameraAttachment"+msg.id);
+                q.innerHTML="Camera "+msg.camid+" attached to texture "+msg.texname;
+                div.appendChild(q);
+            }
+        }else {
+            destroyX(msg,msg.texname+"CameraAttachment");
+        }
+    }
+    this.methodTable["DestroyCamera"]=function(msg) {
+        destroyX(msg,"Camera");
+    }
+
+    this.methodTable["IFrame"]=function(msg) {
+
+        var div=returnObjById(msg.id);
+        if (div) {
+            var q=returnObjById("IFrame"+msg.id);        
+            if(q) {
+                
+            }else {
+                q=document.createElement("iframe");                
+                q.id="IFrame"+msg.id;
+            }
+            q.setAttribute("src",msg.uri);
+            div.appendChild(q);
+        }        
+    }
+    this.methodTable["DestroyIFrame"]=function(msg) {
+        destroyX(msg,"IFrame");
+    }
+    
     this.send=function(obj) {
         return this.methodTable[obj.msg](obj);
     }
