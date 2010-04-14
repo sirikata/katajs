1cb1f6f70947584bd187d9de74722c70ac653370
Patrick Horn <patrick.horn@gmail.com>
2010-04-13 22:21:50 -0700

    Use Kata.extend in CameraScript.js

--- network/sirikata/CameraScript.js
+++ network/sirikata/CameraScript.js
@@ -3,14 +3,15 @@ Kata.include("../externals/protojs/protobuf.js");
 Kata.include("../externals/protojs/pbj.js");
 Kata.include("sirikata/BasicScript.js");
 (function() {
-     Sirikata.CameraScript = function(channel,args){
-         Sirikata.BasicScript.call(this,channel,args);
-         channel.sendMessage({msg:"Proximity",spaceid:args.spaceid,radius:1.0e+8});
-     };
-     Sirikata.CameraScript.prototype.processMessage=function(channel,msg){
-         Sirikata.BasicScript.prototype.processMessage.call(this,channel,msg);
-         
-     };
+    var SUPER = Sirikata.BasicScript.prototype;
+    Sirikata.CameraScript = function(channel,args){
+        SUPER.constructor.call(this,channel,args);
+        channel.sendMessage({msg:"Proximity",spaceid:args.spaceid,radius:1.0e+8});
+    };
+    Kata.extend(Sirikata.CameraScript, SUPER);
+    Sirikata.CameraScript.prototype.processMessage=function(channel,msg){
+        SUPER.processMessage.call(this,channel,msg);
+    };
 })();
 
 
