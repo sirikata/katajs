485a477b47d718d02757e46ae97fc91a9f9f197e
dbm <dbm@kata-wallenberg.(none)>
2010-07-27 14:34:11 -0700

    camera subscribed to remotes now we get remote presence updates (in Script) next: add _handlePresenceLocUpdate to GraphicsScript

--- examples/simple_script/BlessedScript.js
+++ examples/simple_script/BlessedScript.js
@@ -21,16 +21,20 @@ Kata.include("katajs/oh/Script.js");
     Kata.extend(Example.BlessedScript, SUPER);
     Example.BlessedScript.prototype.proxEvent = function(remote, added){
         console.log("debug BlessedScript proxEvent remote:", remote.id(), added)
-        if (added)
+        if (added) {
             Kata.warn("Camera Discover object.");
-        else 
-            Kata.warn("Camera Wiped object.");
+            this.mPresence.subscribe(remote.id())
+        }
+        else {
+            Kata.warn("Camera Wiped object.");      // FIXME: unsubscribe!
+        }
     };
     Example.BlessedScript.prototype.connected = function(presence){
         console.log("debug BlessedScript connected presence:", presence.id())
         this.enableGraphicsViewport(presence, 0);
         presence.setQueryHandler(Kata.bind(this.proxEvent, this));
         presence.setQuery(0);
+        this.mPresence=presence;
         Kata.warn("Got connected callback.");
     };
 })();
