7a1590e974c703cfa03999a0f278282676da2c62
daniel b miller <dbm@ubuntu.(none)>
2010-07-29 23:02:43 -0700

    improved test function, uses its own scripts

---
+++ tests/envjs/test_Blessed.js
@@ -0,0 +1,38 @@
+
+Kata.include("katajs/oh/Script.js");
+
+(function(){
+    if (typeof(Example) === "undefined") {
+        Example = {};
+    }
+    
+    var SUPER = Kata.GraphicsScript.prototype;
+    Example.BlessedScript = function(channel, args){
+        SUPER.constructor.call(this, channel, args);
+        
+        this.connect(args.space, null, Kata.bind(this.connected, this));
+        
+        for (var idx = 0; idx < 2; idx++) {
+            this.createObject("tests/envjs/test_Script.js", "Example.TestScript", {
+                space: args.space
+            });
+        }
+    };
+    Kata.extend(Example.BlessedScript, SUPER);
+    Example.BlessedScript.prototype.proxEvent = function(remote, added){
+        if (added) {
+            Kata.warn("Camera Discover object.");
+            this.mPresence.subscribe(remote.id())
+        }
+        else {
+            Kata.warn("Camera Wiped object.");      // FIXME: unsubscribe!
+        }
+    };
+    Example.BlessedScript.prototype.connected = function(presence){
+        this.enableGraphicsViewport(presence, 0);
+        presence.setQueryHandler(Kata.bind(this.proxEvent, this));
+        presence.setQuery(0);
+        this.mPresence=presence;
+        Kata.warn("Got connected callback.");
+    };
+})();
---
+++ tests/envjs/test_Script.js
@@ -0,0 +1,46 @@
+
+Kata.include("katajs/oh/Script.js");
+
+(function(){
+    if (typeof(Example) === "undefined") {
+        Example = {};
+    }
+    
+    var SUPER = Kata.GraphicsScript.prototype;
+    Example.TestScript = function(channel, args){
+        SUPER.constructor.call(this, channel, args);
+        
+        this.mNearby = {};
+        this.connect(args.space, null, Kata.bind(this.connected, this));
+        this.instance = Example.TestScript.instance;
+        this.movecount = 0
+        this.movemax = 1
+        Example.TestScript.instance += 1
+    };
+    Kata.extend(Example.TestScript, SUPER);
+    
+    Example.TestScript.prototype.connected = function(presence){
+        this.mPresence = presence;
+        // Start periodic movements
+        this.move();
+    };
+    
+    Example.TestScript.prototype.move = function(){
+        if (!this.mPresence) 
+            return;
+        
+        var pos = this.mPresence.position();
+        pos[0] = this.instance * 3;
+        pos[1] = this.movecount * 3
+        pos[2] = -10
+        console.log("move mPresence.setPosition:", pos[0], pos[1], pos[2])
+        this.mPresence.setPosition(pos);
+        
+        // disabled until updates work
+        if (this.movecount < this.movemax) {
+            this.movecount++;
+            setTimeout(Kata.bind(this.move, this), 2000);
+        }
+    };
+    Example.TestScript.instance = 0;
+})();
