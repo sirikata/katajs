666e91073c95a769ac16b4c8ab205a7886521ae4
Patrick Horn <patrick.horn@gmail.com>
2010-10-31 18:08:34 -0700

    Deleted compiled closure

--- katajs/oh/plugins/sirikata/impl/Empty.pbj.js
+++
@@ -1,6 +0,0 @@
-if (typeof(Sirikata)=="undefined") {Sirikata = {};}
-if (typeof(Sirikata.Protocol)=="undefined") {Sirikata.Protocol = {};}
-Sirikata.Protocol._PBJ_Internal="pbj-0.0.3";
-
-Sirikata.Protocol.Empty = PROTO.Message("Sirikata.Protocol.Empty",{
-});
