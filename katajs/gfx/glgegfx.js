660be70a1eb1eecd0c69695e1892cf060cb9f418
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2010-11-22 23:08:52 -0800

    scale is applied after transform, so transform should be in meters, not in collada units

--- katajs/gfx/glgegfx.js
+++ katajs/gfx/glgegfx.js
@@ -210,9 +210,9 @@ Kata.require([
                                  clda.setScaleX(maxv?scale[0]*colladaUnitRescale:1);
                                  clda.setScaleY(maxv?scale[1]*colladaUnitRescale:1);
                                  clda.setScaleZ(maxv?scale[2]*colladaUnitRescale:1);
-                                 clda.setLocX(offset[0]-bv.center[0]);
-                                 clda.setLocY(offset[1]-bv.center[1]);
-                                 clda.setLocZ(offset[2]-bv.center[2]);            
+                                 clda.setLocX(offset[0]-(bv.center[0])*colladaUnitRescale);
+                                 clda.setLocY(offset[1]-(bv.center[1])*colladaUnitRescale);
+                                 clda.setLocZ(offset[2]-(bv.center[2])*colladaUnitRescale);            
 
                              }
                              gfx._inputCb({msg:"loaded",id:thus.mID});
