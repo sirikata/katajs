054c24152bf50cf19bbf5d73d979c6d0e4ad1aa9
danx0r <danbmil99@gmail.com>
2010-11-25 13:53:38 -0800

    kluge to put avatars above ground

--- katajs/gfx/glgegfx.js
+++ katajs/gfx/glgegfx.js
@@ -213,7 +213,7 @@ Kata.require([
             clda.setScaleY(maxv?scale[1]*colladaUnitRescale:1);
             clda.setScaleZ(maxv?scale[2]*colladaUnitRescale:1);
             clda.setLocX(offset[0]-(bv.center[0])*colladaUnitRescale);
-            clda.setLocY(offset[1]-(bv.center[1])*colladaUnitRescale);
+            clda.setLocY(offset[1]-(bv.center[1])*colladaUnitRescale + 1);
             clda.setLocZ(offset[2]-(bv.center[2])*colladaUnitRescale);            
             gfx._inputCb({msg:"loaded",id:thus.mID});
             clda.removeEventListener("loaded",loadedCallback);
